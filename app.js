e29a05a50c07787b980070c829278d4656234458
dy93 <dy93_@hotmail.com>
2017-12-24 23:24:14 +0800

    Add eslintrc and fix styles

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
+const Koa = require('koa');
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const app = new Koa();
+const views = require('koa-views');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+const logger = require('koa-logger');
+
+const index = require('./routes/index');
+const users = require('./routes/users');
 
 // error handler
-onerror(app)
+onerror(app);
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+  enableTypes: ['json', 'form', 'text'],
+}));
+app.use(json());
+app.use(logger());
+app.use(require('koa-static')(`${__dirname}/public`));
 
-app.use(views(__dirname + '/views', {
-  extension: 'ejs'
-}))
+app.use(views(`${__dirname}/views`, {
+  extension: 'ejs',
+}));
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
 
 // routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  console.error('server error', err, ctx);
 });
 
-module.exports = app
+module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
-const router = require('koa-router')()
+const router = require('koa-router')();
 
-router.prefix('/users')
+router.prefix('/users');
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
-})
+router.get('/', (ctx, next) => {
+  ctx.body = 'this is a users response!';
+});
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
+router.get('/bar', (ctx, next) => {
+  ctx.body = 'this is a users/bar response';
+});
 
-module.exports = router
+module.exports = router;
