67a6ee8b33420f2bb09ed7d08114a7381a40d509
dy93 <dy93_@hotmail.com>
2018-05-07 23:48:50 +0800

    Setup debug setting

--- src/app.ts
+++ src/app.ts
@@ -41,9 +41,9 @@ app.use(bodyparser({
 }));
 app.use(json());
 app.use(logger());
-app.use(require('koa-static')(`${__dirname}/public`));
+app.use(require('koa-static')(`${__dirname}/../public`));
 
-app.use(views(`${__dirname}/views`, {
+app.use(views(`${__dirname}/../views`, {
   extension: 'ejs'
 }));
 
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const pics = fs.readdirSync(path.resolve(__dirname, './public/images')).filter(f => f.startsWith('Ali')).map(f => `/images/${f}`);
+const pics = fs.readdirSync(path.resolve(__dirname, '../public/images')).filter(f => f.startsWith('Ali')).map(f => `/images/${f}`);
 
 module.exports = {
   pic: {
