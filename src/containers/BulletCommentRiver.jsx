01172cc3ab34af844e38fb19a8a28dcfdf756c52
ueewbd@hotmail.com <ueewbd@hotmail.com>
2018-05-21 15:30:00 +0800

    Use css transition to implement bullet comment

--- src/containers/BulletCommentRiver.jsx
+++ src/containers/BulletCommentRiver.jsx
@@ -1,74 +1,95 @@
 import React from 'react';
+import { Transition } from 'react-transition-group';
 
-const STYLE = {
+const defaultStyle = {
   position: 'absolute',
-  left: 'auto',
+  right: '0px',
   whiteSpace: 'nowrap',
-  textShadow: '-1px 0 white, 0 1px white, 1px 0 white, 0 -1px white'
+  width: '0%',
+  transitionTimingFunction: 'linear',
+  transitionProperty: 'width',
+  textShadow: '-1px 0 whitesmoke, 0 1px whitesmoke, 1px 0 whitesmoke, 0 -1px whitesmoke',
+  textAlign: 'left',
+  overflow: 'hidden',
+  color: 'dimgray'
 }
 
-function calculate(candidates) {
-  const sum = candidates.reduce((pre, cur) => pre + cur, 0);
+const transitionStyles = {
+  entering: { width: '0%' },
+  entered: { width: '500%' }
+};
+
+function Comment(props) {
+  const { top, text, duration } = props;
+  return (
+    <Transition in appear timeout={100}>
+      {(state) => (
+        <span style={{
+          ...defaultStyle,
+          ...transitionStyles[state],
+          top,
+          transitionDuration: `${duration}s`
+        }}>
+          {text}
+        </span>
+      )}
+    </Transition>
+  );
+}
+
+function calculate(slotWeight) {
+  const sum = slotWeight.reduce((pre, cur) => pre + cur, 0);
   let rand = Math.floor(Math.random() * sum);
-  for (let i = 0; i < candidates.length; i += 1) {
-    if (rand < candidates[i]) {
+  for (let i = 0; i < slotWeight.length; i += 1) {
+    if (rand < slotWeight[i]) {
       return i;
     }
-    rand = rand - candidates[i];
+    rand = rand - slotWeight[i];
   }
-  return candidates.length - 1;
+  return slotWeight.length - 1;
 }
 
 class BulletCommentRiver extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      candidates: new Array(32).fill(64),
+      slotWeight: new Array(32).fill(64),
       comments: []
     }
   }
-  componentDidMount() {
-    this.handler = setInterval(() => {
-      this.setState((preState) => {
-        const comments = preState.comments
-          .filter(({ offset }) => offset < 10000)
-          .map(comment => ({ ...comment, offset: comment.offset + comment.step }));
-        return { ...preState, comments };
-      })
-    }, 60)
-  }
-  componentWillUnmount() {
-    if (this.handler) {
-      clearInterval(this.handler);
-    }
-  }
   componentWillReceiveProps(nextProps) {
-    if (this.props.newComment !== nextProps.newComment) {
+    if (this.props.newComment !== nextProps.newComment && nextProps.newComment) {
       this.setState((preState) => {
-        const chooseSlot = calculate(preState.candidates);
-        const candidates = preState.candidates.map((v, i) => {
+        const chooseSlot = calculate(preState.slotWeight);
+        const slotWeight = preState.slotWeight.map((v, i) => {
           if (i === chooseSlot) {
             return Math.max(v - 31, 0);
           }
           return v + 1;
         });
-        const comments = [...preState.comments];
-        const step = Math.floor(Math.random() * 5) + 1;
-        comments.push({ ...nextProps.newComment, slot: chooseSlot, offset: 0, step });
-        return { candidates, comments };
+        const now = Date.now();
+        const comments = preState.comments.filter(({ createAt }) => now - createAt < 80000);
+        const duration = Math.max(-10, Math.floor(nextProps.newComment.content.length / -5)) * 5 + 80;
+        comments.push({
+          ...nextProps.newComment,
+          duration,
+          top: `${(chooseSlot + 1) * 2.5}%`,
+          createAt: Date.now()
+        });
+        return { slotWeight, comments };
       })
     }
   }
   render() {
     const { comments } = this.state;
     return (
-      <div style={{ overflow: 'hidden' }}>
-        {comments.map((comment, i) => (
-          <span
-            key={comment.id} 
-            style={{ ...STYLE, right: `${comment.offset}px`, top: `${(comment.slot + 1) * 2.5}%` }}>
-            {comment.content}
-          </span>
+      <div>
+        {comments.map((comment) => (
+          <Comment
+            key={comment.id}
+            text={comment.content}
+            top={comment.top}
+            duration={comment.duration} />
         ))}
       </div>
     );
