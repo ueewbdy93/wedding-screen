57c49e4035a1d2b9f4575ef31bc226316257dcb4
dy93 <dy93_@hotmail.com>
2018-05-24 01:04:29 +0800

    Set the frontend path and image cache

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,10 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, '../public')));
+app.use(express.static(join(__dirname, '../public'),  {
+  maxAge: '2h',
+}));
+app.use(express.static(join(__dirname, '../frontend/build')));
 
 app.use('/', index);
 app.use('/users', users);
