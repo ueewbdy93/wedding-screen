7bdc70552c90d06031e5de39dcfe67a1282eaa50
ueewbd@hotmail.com <ueewbd@hotmail.com>
2018-05-24 16:16:41 +0800

    Add margin bottom to comment input

--- src/containers/CommentInput.jsx
+++ src/containers/CommentInput.jsx
@@ -10,6 +10,7 @@ const STYLE = {
 }
 
 const INPUT_STYLE = {
+  marginBottom: '20px',
   boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
   width: '70%',
   textAlign: 'center',
