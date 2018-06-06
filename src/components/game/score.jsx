7bccac12099614cfb04d665f4ae2f5a122e5784c
ueewbd@hotmail.com <ueewbd@hotmail.com>
2018-06-06 18:48:33 +0800

    Fix bug that score shows 0 if rank > 10

--- src/components/game/score.jsx
+++ src/components/game/score.jsx
@@ -4,7 +4,8 @@ import './score.css';
 
 function score(props) {
   const { player, rank } = props;
-  const myRank = rank.findIndex(entry => entry.id === player.id);
+  const myRankIndex = rank.findIndex(entry => entry.id === player.id);
+  const myRank = rank[myRankIndex];
   const top10 = rank.slice(0, 10);
   return (
     <Container>
@@ -15,15 +16,15 @@ function score(props) {
             <tr><td><small>名次</small></td><td><small>名字</small></td><td><small>分數</small></td></tr>
             {
               top10.map((playerScore, i) => (
-                <tr key={i} className={i === myRank ? 'me' : ''}>
+                <tr key={i} className={i === myRankIndex ? 'me' : ''}>
                   <td>{i + 1}</td>
                   <td>{playerScore.name.length > 8 ? `${playerScore.name.slice(0, 8)}...` : playerScore.name}</td>
                   <td>{playerScore.score}</td>
                 </tr>
               ))
             }
-            {myRank > 10 && <tr><td colSpan={3}>...</td></tr>}
-            {myRank >= 10 && <tr className="me"><td>{myRank + 1}</td><td>{player.name}</td><td>{player.score}</td></tr>}
+            {myRankIndex > 10 && <tr><td colSpan={3}>...</td></tr>}
+            {myRankIndex >= 10 && <tr className="me"><td>{myRankIndex + 1}</td><td>{myRank.name}</td><td>{myRank.score}</td></tr>}
           </tbody>
         </table>
       </Content>
