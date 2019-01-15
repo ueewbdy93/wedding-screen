86a5d74d47a4c9743b84babf63da20e38d9dd322
david <david@mediot.com.tw>
2019-01-16 00:59:37 +0800

    issue #10 - Move frontend codes

--- webpack/utils/config.js
+++ webpack/utils/config.js
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export default {
-  userEntry: path.resolve(__dirname, './../../src/frontend/src/index.js'),
-  adminEntry: path.resolve(__dirname, './../../src/frontend/src/admin-index.js'),
+  userEntry: path.resolve(__dirname, './../../src/frontend/index.js'),
+  adminEntry: path.resolve(__dirname, './../../src/frontend/admin-index.js'),
   path: path.resolve(__dirname + './../../dist/public'),
 };
